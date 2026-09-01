a67c62e93c370edd73eb3637ebe3e42d8d8d0eef
Emmanuel Michel <emmanuel.michel12@gmail.com>
2026-09-01 15:10:10 -0400

    Add: path-sum - submission-0

---
+++ Data Structures & Algorithms/path-sum/submission-0.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+    
+        if(root == null)
+        {
+            return false;
+        }
+
+        int val = targetSum - root.val;
+
+        if(root.left == null && root.right == null)
+        {
+            return val == 0;
+        }
+
+
+        return hasPathSum(root.left, val) || hasPathSum(root.right, val);
+    }
+}
